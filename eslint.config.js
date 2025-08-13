fdb4d0486eab7a09678bbee5f97432b10e1b8e9c
tavolafourcade <tavolafourcade@gmail.com>
2025-08-12 20:13:09 -0500

    feat(app): clean boilerplate

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -20,4 +21,6 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
+  // Disable rules that conflict with Prettier formatting
+  prettier,
 ])
